4080080c03a9d445ec0106730ca79c574bf173f6
lingbend <lingbend@gmail.com>
2025-01-22 00:00:25 -0700

    Implemented ChessPiece getter functions

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -49,14 +49,15 @@ public class ChessPiece {
      * @return Which team this chess piece belongs to
      */
     public ChessGame.TeamColor getTeamColor() {
-        throw new RuntimeException("Not implemented");
+        return this.teamcolor;
+
     }
 
     /**
      * @return which type of chess piece this piece is
      */
     public PieceType getPieceType() {
-        throw new RuntimeException("Not implemented");
+        return this.piecetype;
     }
 
     /**
