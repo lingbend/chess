0ca274bdbae1c7f7b4f8ffd1626c092cd270232e
lingbend <lingbend@gmail.com>
2025-02-10 17:33:59 -0700

    Added getBoard and setBoard to ChessGame

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -123,7 +123,7 @@ public class ChessGame {
      * @param board the new board to use
      */
     public void setBoard(ChessBoard board) {
-        throw new RuntimeException("Not implemented");
+        game_board = new ChessBoard(board);
     }
 
     /**
@@ -132,6 +132,6 @@ public class ChessGame {
      * @return the chessboard
      */
     public ChessBoard getBoard() {
-        throw new RuntimeException("Not implemented");
+        return game_board;
     }
 }
