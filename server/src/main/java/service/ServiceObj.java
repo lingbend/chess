b5959515139144428dcb9b4877b2d6b51723bd48
lingbend <lingbend@gmail.com>
2025-02-15 22:46:27 -0700

    Added ServiceObj interface

---
+++ server/src/main/java/service/ServiceObj.java
@@ -0,0 +1,14 @@
+package service;
+
+public interface ServiceObj {
+    String GetAuthToken();
+    void SetAuthToken(String token);
+    String GetGameName();
+    void SetGameName(String name);
+    String GetGameID();
+    void SetGameID(String ID);
+    String GetUsername();
+    void SetUsername(String username);
+
+
+}
