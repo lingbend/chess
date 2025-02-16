7ebdc3186c1e59417dff4fbfd1682aa7cf66e78c
lingbend <lingbend@gmail.com>
2025-02-16 00:30:43 -0700

    Drafted dataAccess interface

--- server/src/main/java/dataAccess/dataAccess.java
+++ server/src/main/java/dataAccess/dataAccess.java
@@ -1,4 +1,13 @@
 package dataAccess;
 
-public class dataAccess {
+import java.lang.String;
+
+public interface dataAccess {
+    boolean Create(Object obj);
+    boolean Find(Object index);
+    boolean Update(String index);
+    boolean Delete(Object index);
+    boolean Match(Object index);
+    Object Read(String index);
+    boolean DeleteAll(Class type);
 }
