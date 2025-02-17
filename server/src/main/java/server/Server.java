9689b09430d3a70098e82f6bb81f2d6b3f96b2d2
lingbend <lingbend@gmail.com>
2025-02-17 15:41:50 -0700

    Finished drafting registration process

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,6 +1,8 @@
 package server;
 
 import spark.*;
+import service.*;
+import handler.*;
 
 public class Server {
 
@@ -10,7 +12,7 @@ public class Server {
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
-
+        Spark.post("/user", this::register);
         //This line initializes the server and can be removed once you have a functioning endpoint
         Spark.init();
 
@@ -21,5 +23,12 @@ public class Server {
     public void stop() {
         Spark.stop();
         Spark.awaitStop();
+    }
+
+    private Object register(Request req, Response res) {
+        var service =  new RegisterService();
+        var handler = new JsonHandler(service);
+        service.registerHandler(handler);
+        return handler.Deserialize(req.body());
     }
 }
