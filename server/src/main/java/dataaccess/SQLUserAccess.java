5c5398216fdbe99f3d1b2f497f8f94ffc7649b39
lingbend <lingbend@gmail.com>
2025-03-12 21:45:03 -0600

    Drafted SQLUserAccess

--- server/src/main/java/dataaccess/SQLUserAccess.java
+++ server/src/main/java/dataaccess/SQLUserAccess.java
@@ -1,10 +1,105 @@
 package dataaccess;
 
+import model.UserData;
+
 public class SQLUserAccess implements DataAccess{
-    public boolean create(Object obj){};
-    public boolean find(Object index){};
-    public boolean update(Object index){};
-    public boolean delete(Object index){};
-    public Object read(String index){};
-    public boolean deleteAll(){};
+
+    public SQLUserAccess(){}
+
+    public boolean create(Object obj){
+        UserData userData = (UserData) obj;
+
+        try (var conn = DatabaseManager.getConnection()) {
+            String statement = "INSERT users (username, email, password) VALUES(?, ?, ?)";
+            var preparedStatement = conn.prepareStatement(statement);
+
+            preparedStatement.setString(1, userData.getUsername());
+            preparedStatement.setString(2, userData.getEmail());
+            preparedStatement.setString(3, userData.getPassword());
+
+            preparedStatement.executeUpdate();
+
+            return true;
+        }
+        catch (Exception ex) {
+            return false;
+        }
+
+
+    };
+    public boolean find(Object index){
+        String username = (String) index;
+
+        try (var conn = DatabaseManager.getConnection()) {
+            String statement = "SELECT username FROM users WHERE username=?";
+            var preparedStatement = conn.prepareStatement(statement);
+
+            preparedStatement.setString(1, username);
+
+            var userRecord = preparedStatement.executeQuery();
+
+            while(userRecord.next()) {
+                if (userRecord.getString("username").equals(username)) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        catch (Exception ex) {
+            return false;
+        }
+
+
+    };
+
+    public boolean update(Object index){
+        return false;
+    };
+
+    public boolean delete(Object index){
+        return false;
+    };
+
+    public Object read(String index){
+        String username = (String) index;
+
+        try (var conn = DatabaseManager.getConnection()) {
+            String statement = "SELECT username, email, password FROM users WHERE username=?";
+            var preparedStatement = conn.prepareStatement(statement);
+
+            preparedStatement.setString(1, username);
+
+            var userRecord = preparedStatement.executeQuery();
+
+            if (userRecord.next()) {
+                return new UserData(username, userRecord.getString("password"),
+                        userRecord.getString("email"));
+            }
+            else {
+                return false;
+            }
+        }
+
+        catch (Exception ex) {
+            return null;
+        }
+
+    };
+    public boolean deleteAll(){
+        try (var conn = DatabaseManager.getConnection()) {
+            String statement = "DROP TABLE users";
+            var preparedStatement = conn.prepareStatement(statement);
+
+            preparedStatement.executeUpdate();
+
+            return true;
+
+        }
+
+        catch (Exception ex) {
+            return false;
+        }
+    };
 }
