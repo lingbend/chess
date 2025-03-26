7731f33359ea1742a98e10d0d5008a891c92103c
lingbend <lingbend@gmail.com>
2025-03-25 23:53:17 -0600

    Updated ServerFacadeInterface parameter type to State enum

--- client/src/main/java/ui/ServerFacadeInterface.java
+++ client/src/main/java/ui/ServerFacadeInterface.java
@@ -7,15 +7,15 @@ import java.util.ArrayList;
 
 public interface ServerFacadeInterface {
     void run();
-    String getHelp(Enum state);
-    void quit(Enum state);
-    String login(Enum state, String username, String password);
-    String register(Enum state, String username, String password, String email);
-    boolean logout(Enum state, String authToken);
-    String createGame(Enum state, String authToken, String gameName);
-    ArrayList<GameData> listGames(Enum state, String authToken);
-    String playGame(Enum state, String authToken, int gameNumber, ChessGame.TeamColor color);
-    boolean observeGame(Enum state, String authToken, int gameNumber);
+    String getHelp(State state);
+    void quit(State state);
+    String login(State state, String username, String password);
+    String register(State state, String username, String password, String email);
+    boolean logout(State state, String authToken);
+    String createGame(State state, String authToken, String gameName);
+    ArrayList<GameData> listGames(State state, String authToken);
+    String playGame(State state, String authToken, int gameNumber, ChessGame.TeamColor color);
+    boolean observeGame(State state, String authToken, int gameNumber);
 
     enum State {
         LoggedOut,
