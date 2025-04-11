1affac1bdc46aa1e3d0432efb04b06744c7f7895
lingbend <lingbend@gmail.com>
2025-04-10 23:47:28 -0600

    Drafted preview moves and redraw InGameUI functions

--- client/src/main/java/ui/InGameUI.java
+++ client/src/main/java/ui/InGameUI.java
@@ -1,7 +1,10 @@
 package ui;
 
 import chess.ChessGame;
+import chess.ChessPosition;
 
+import java.lang.Math;
+import java.lang.Character;
 import java.util.ArrayList;
 
 public class InGameUI {
@@ -19,14 +22,17 @@ public class InGameUI {
             System.out.print(getHelp());
         }
         else if (command.equals("redraw")) {
-            System.out.println("");
+            System.out.println("Redrawing...");
+            System.out.println(clientDB.drawer.drawBoard(clientDB.currentGame.getGame(), null));
         }
         else if (command.equals("leave")) {
             System.out.println("");
         }
         else if (command.equals("move")) {
             checkParameters(2);
-            System.out.println("");
+            System.out.println("Previewing moves...");
+            ChessPosition start = parsePosition(parameters.get(0));
+            System.out.println(clientDB.drawer.drawBoard(clientDB.currentGame.getGame(), start));
         }
         else if (command.equals("preview")) {
             checkParameters(1);
@@ -55,6 +61,35 @@ public class InGameUI {
                 View legal moves for the piece on a square          | preview space(eg. e4)
                 View currently available options                    | help
                 """;
+    }
+
+    private ChessPosition parsePosition(String pos) throws IllegalArgumentException{
+        if (pos.length() != 2) {
+            throw new IllegalArgumentException("Position parameter must be 2 letters long");
+        }
+
+        char a = pos.charAt(0);
+        char b = pos.charAt(1);
+        int row;
+        int col;
+
+        if (Character.isAlphabetic(a) && Character.isDigit(b)) {
+            col = Character.getNumericValue(a) - 9;
+            row = Character.getNumericValue(b);
+        }
+        else if (Character.isAlphabetic(b) && Character.isDigit(a)) {
+            row = Character.getNumericValue(b) - 9;
+            col = Character.getNumericValue(a);
+        }
+        else {
+            return null;
+        }
+
+        if (Math.abs(row) > 8 || Math.abs(col) > 8) {
+            throw new IllegalArgumentException("Position must use letters a-h and 1-8");
+        }
+
+        return new ChessPosition(row, col);
     }
 
 
